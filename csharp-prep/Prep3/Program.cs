7de3d9e0d2711ac01d968e8d7fab5b49272bceb7
saraltr <saralatorre97@gmail.com>
2023-01-13 13:33:34 +0100

    prep added

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int userNumber = 0;
+        int guessCount = 1;
+
+        bool game = true;
+
+        while (game)
+        {
+            do
+            {
+                Console.Write("What is the magic number? ");
+                userNumber = int.Parse(Console.ReadLine());
+
+                if (magicNumber > userNumber)
+                {
+                    Console.WriteLine("Higher");
+                    guessCount += 1;
+                }
+                else if (magicNumber < userNumber)
+                {
+                    Console.WriteLine("Lower");
+                    guessCount += 1;
+
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} times.");
+                    Console.WriteLine("Would you like to play again? Enter yes or no: ");
+                    string option = Console.ReadLine();
+
+                    if (option == "yes")
+                    {
+                        game = true;
+                    }
+                }
+
+            } while (userNumber != magicNumber);
+        }
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
     }
 }
