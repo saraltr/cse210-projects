825e00146d74cd7b4458d625b9d97163ecf7d584
saraltr <saralatorre97@gmail.com>
2023-02-12 05:53:55 +0100

    new scriptures added

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,17 +5,44 @@ public class Program
 {
     public static void Main()
     {
-        Reference reference = new Reference("John", 3, 16);
-        List<string> text = new List<string>
+        List<Reference> references = new List<Reference>
+    {
+        new Reference("John", 3, 16),
+        new Reference("Matthew", 5, 44),
+        new Reference("Mark", 12, 31),
+        new Reference("Alma", 7, 11, 12)
+    };
+        List<List<string>> texts = new List<List<string>>
+    {
+        new List<string>
         {
             "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.",
             "For God did not send his Son into the world to condemn the world, but to save the world through him."
-        };
+        },
+        new List<string>
+        {
+            "Therefore, I tell you, love your enemies and pray for those who persecute you,",
+            "so that you may be children of your Father in heaven."
+        },
+        new List<string>
+        {
+            "Love the Lord your God with all your heart and with all your soul and with all your mind.",
+            "This is the first and greatest commandment."
+        },
+        new List<string>
+        {
+            "And he will take upon him death, that he may loose the bands of death which bind his people; and he will take upon him their infirmities, that his bowels may be filled with mercy, according to the flesh, that he may know according to the flesh how to succor his people according to their infirmities"
+        }
+    };
 
+        Random random = new Random();
+        int index = random.Next(references.Count);
+        Reference reference = references[index];
+        List<string> text = texts[index];
         Scripture scripture = new Scripture(reference, text);
+
         reference.DisplayReference();
         scripture.DisplayScripture();
-
         Console.WriteLine("\nPress enter to continue of type 'quit' to finish");
         string input = Console.ReadLine();
 
