c5f677830dcf84a14c04816e429267bfdfee96dd
sara latorre <saralatorre97@gmail.com>
2023-06-09 23:13:44 +0200

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numbers = new List<float>();
+        float userNumber = -1;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            string unserResponse = Console.ReadLine();
+            userNumber = int.Parse(unserResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber != 0);
+
+        // compute the sum of the numbers in the list.
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        // compute the average of the numbers in the list.
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // find the largest number in the list.
+        float largest = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        
+        Console.WriteLine($"The largest number is: {largest}");
+        float smallestPositive = float.MaxValue;
+        foreach (float number in numbers)
+        {
+            if (number > 0 && number < smallestPositive)
+            {
+                smallestPositive = number;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        
+        numbers.Sort();
+        foreach (float x in numbers)
+        {
+            Console.WriteLine(x);
+        }
     }
 }
