25f9ec60c528217e1e72e8c847028019cf1fe016
sara latorre <saralatorre97@gmail.com>
2023-06-09 22:46:10 +0200

    finished prep 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -53,11 +53,11 @@ class Program
 
         if (percent >= 70)
         {
-            Console.WriteLine("You passed!");
+            Console.WriteLine("You passed the class!");
         }
         else
         {
-            Console.WriteLine("Better luck next time!");
+            Console.WriteLine("Keep working hard and you'll improve!");
         }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int userNumber = 0;
+        int guessCount = 1;
+
+        bool game = true;
+
+        while (game)
+        {
+            do
+            {
+                Console.Write("What is the magic number? ");
+                userNumber = int.Parse(Console.ReadLine());
+
+                if (magicNumber > userNumber)
+                {
+                    Console.WriteLine("Higher");
+                    guessCount += 1;
+                }
+                else if (magicNumber < userNumber)
+                {
+                    Console.WriteLine("Lower");
+                    guessCount += 1;
+
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! The secret number was {magicNumber}");
+                    Console.WriteLine($"It took you {guessCount} times.");
+                    Console.WriteLine("Would you like to play again? Enter yes or no: ");
+                    string option = Console.ReadLine();
+
+                    if (option == "yes")
+                    {
+                        magicNumber = randomGenerator.Next(1, 101);
+                        guessCount = 1;
+                        game = true;
+                    } else
+                    {
+                        Console.Write("Thank you for playing! Bye!");
+                        game = false;
+                    }
+                }
+
+            } while (userNumber != magicNumber);
+        }
     }
 }
