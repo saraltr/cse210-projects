d16610ebeef385fe67d6ce9b271bedcc9f3f8b62
sara latorre <saralatorre97@gmail.com>
2023-06-20 15:38:44 +0200

    finished learning activity 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,22 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        f1.GetFractionString();
+        f1.GetDecimalValue();
+
+        Fraction f2 = new Fraction(5);
+        f2.GetFractionString();
+        f2.GetDecimalValue();
+
+        Fraction f3 = new Fraction(3, 4);
+        f3.GetFractionString();
+        f3.GetDecimalValue();
+
+        Fraction f4 = new Fraction(1, 3);
+        f4.GetFractionString();
+        f4.GetDecimalValue();
     }
 }
