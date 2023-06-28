b65cf616718f66fb68ab173916b61525adf43ecd
sara latorre <saralatorre97@gmail.com>
2023-06-28 22:00:41 +0200

    inheritance prep completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        string assignment1 = assignment.GetSummary();
+        Console.WriteLine(assignment1);
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        string assignment2 = mathAssignment.GetSummary();
+        string homeworkList = mathAssignment.GetHomeworkList();
+        Console.WriteLine($"{assignment2} | {homeworkList} ");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        string assignment3 = writingAssignment.GetSummary();
+        string title = writingAssignment.GetWritingInformation();
+        Console.WriteLine($"{assignment3} | {title}");
     }
 }
