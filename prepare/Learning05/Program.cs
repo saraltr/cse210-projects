e5845ca222ba7660337d0621892f29f340a72cf5
sara latorre <saralatorre97@gmail.com>
2023-07-05 09:56:27 +0200

    polymorphism activity completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sq = new Square("blue", 8);
+        shapes.Add(sq);
+
+        Rectangle rect = new Rectangle("yellow", 5, 7);
+        shapes.Add(rect);
+
+        Circle circle = new Circle("red", 6);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Shape color: {color}, Shape area: {area}");
+
+        }
     }
 }
